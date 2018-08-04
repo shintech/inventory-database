13f782bceac677b4a1ef9c19c7b66e41d0964907
Michael Prather <mprather1@gmail.com>
2018-08-04 04:34:54 +0000

    posts

--- init.sql
+++ init.sql
@@ -17,3 +17,11 @@ CREATE TABLE users (
   message VARCHAR,
   created_at TIMESTAMP without time zone default (now() at time zone 'utc')
 );
+
+CREATE TABLE posts (
+  ID SERIAL PRIMARY KEY,
+  title VARCHAR,
+  short VARCHAR,
+  body VARCHAR,
+  created_at TIMESTAMP without time zone default (now() at time zone 'utc')
+);
